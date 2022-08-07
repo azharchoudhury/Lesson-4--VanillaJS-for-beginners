fc507dc84c11887af07cc6e009507065f14ea20b
azharchoudhury <azharmehmud11@gmail.com>
2022-08-08 00:54:47 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
 var outputDiv = document.querySelector("#output");
 
-console.log(outputDiv);
 // outputDiv.innerText = "I am powerful!";
 
 
 function clickHandler() {
-    console.log("clicked");
-    console.log("Input: " , txtInput.value);
+    outputDiv.innerText = "ashajahaj " + txtInput.value;
 }
+
 btnTranslate.addEventListener("click", clickHandler);
