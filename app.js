21f696c5c57ce1067c4ad17356c79606eb7244fe
azharchoudhury <azharmehmud11@gmail.com>
2022-08-08 17:08:31 +0530

    completed the mechanism without the design

--- app.js
+++ app.js
@@ -4,9 +4,34 @@ var outputDiv = document.querySelector("#output");
 
 // outputDiv.innerText = "I am powerful!";
 
+// console.log(outputDiv);
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with the server! try again later..");
+}
 
 function clickHandler() {
-    outputDiv.innerText = "ashajahaj " + txtInput.value;
+    // outputDiv.innerText = "ashajahaj " + txtInput.value;
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
 }
+
+
 
 btnTranslate.addEventListener("click", clickHandler);
